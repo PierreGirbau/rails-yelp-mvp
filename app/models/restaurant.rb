3e13a8e72af341d6aaa9d54bd0dbf0e493d072ae
Pierre Girbau <pierregirbau@gmail.com>
2018-02-14 14:35:17 +0100

    Created new view and form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true
-  validates :category, inclusion: { in: ["Chinese", "Italian", "Japanese", "French", "Belgian"] }
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
   validates :address, presence: true
   validates :phone_number, uniqueness: true, presence: true
 end
